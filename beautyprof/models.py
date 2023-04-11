fed817cd57706645b9933c88b1206474b28d7a8a
MinuMThomas <minumarytomas@gmail.com>
2023-04-11 18:41:21 +0000

    Autofill slug field from string.

--- beautyprof/models.py
+++ beautyprof/models.py
@@ -1,19 +1,26 @@
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+# Import slugify to generate slugs from strings
+from django.utils.text import slugify 
+
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
-    last_name = models.CharField(max_length=200, blank=True)
+    profession = models.CharField(max_length=200, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(default="no bio...", max_length=300)
     email = models.EmailField(max_length=200, blank=True)
     county = models.CharField(max_length=200, blank=True)
     avatar = CloudinaryField('image', default='placeholder')
     follower = models.ManyToManyField(User, blank=True, related_name='follower')
-    slug = models.SlugField(unique=True, blank=True)
+    slug = models.SlugField(unique=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
 
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.profession)
+        super(Profile, self).save(*args, **kwargs)
+    
     def __str__(self):
-        return f"{self.user.username}"
+        return f"{self.user.username}"    
