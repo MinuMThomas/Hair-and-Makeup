b364101ddaf8399474682c60fb2dd7e855b05fe8
MinuMThomas <minumarytomas@gmail.com>
2023-04-12 12:15:18 +0000

    Update bio field and delete follower filed.

--- beautyprof/models.py
+++ beautyprof/models.py
@@ -2,18 +2,17 @@ from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
 # Import slugify to generate slugs from strings
-from django.utils.text import slugify 
+from django.utils.text import slugify
 
 
 class Profile(models.Model):
     first_name = models.CharField(max_length=200, blank=True)
     profession = models.CharField(max_length=200, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(default="no bio...", max_length=300)
+    bio = models.TextField(default="add about your work and contact", max_length=300)
     email = models.EmailField(max_length=200, blank=True)
     county = models.CharField(max_length=200, blank=True)
     avatar = CloudinaryField('image', default='placeholder')
-    follower = models.ManyToManyField(User, blank=True, related_name='follower')
     slug = models.SlugField(unique=True)
     updated = models.DateTimeField(auto_now=True)
     created = models.DateTimeField(auto_now_add=True)
@@ -23,4 +22,5 @@ class Profile(models.Model):
         super(Profile, self).save(*args, **kwargs)
     
     def __str__(self):
-        return f"{self.user.username}"    
+        return f"{self.user.username}"
+ 
