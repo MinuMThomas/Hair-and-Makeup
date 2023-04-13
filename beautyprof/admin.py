aca622a5d53d6e7c69e6e17cfc64194d041c2870
MinuMThomas <minumarytomas@gmail.com>
2023-04-13 03:40:25 +0000

    Add list display and search filed in admin panel.

--- beautyprof/admin.py
+++ beautyprof/admin.py
@@ -6,4 +6,7 @@ from django_summernote.admin import SummernoteModelAdmin
 
 @admin.register(Profile)
 class ProfileAdmin(SummernoteModelAdmin):
+    list_filter = ('name', 'created')
     summernote_fields = ('bio')
+    list_display = ('name', 'profession')
+    search_fields = ['profession']
