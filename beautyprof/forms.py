6f047350996d5267bab0b1dcef9178122308724d
MinuMThomas <minumarytomas@gmail.com>
2023-04-18 21:30:01 +0000

    update form.py

--- beautyprof/forms.py
+++ beautyprof/forms.py
@@ -1,11 +1,18 @@
-from .models import Profile
 from django import forms
+from .models import Profile
+from django_summernote.widgets import SummernoteWidget, SummernoteInplaceWidget
+
+
+# class ProfileForm(forms.ModelForm):
+#     class Meta:
+#         model = Profile
+#         fields = ['name', 'profession', 'email', 'avatar', 'bio']
 
 
 class ProfileForm(forms.ModelForm):
     class Meta:
         model = Profile
-        fields = ('name',
-                  'profession',
-                  'email',
-                  'avatar',)
+        fields = ['name', 'profession', 'email', 'avatar', 'bio']
+        widgets = {
+            'content': SummernoteWidget(),
+                    }
